c3f88fc91951b5578dd8da9b4e16b8daabbd976e
Brian Montana <contact@brianmontana.net>
2018-07-22 15:38:44 -0500

    Closing out module 5.17

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
+  req.flash('error', 'Something happened, Error!');
+  req.flash('info', 'Something happened, Info!');
+  req.flash('warning', 'Something happened, Warning!');
+  req.flash('success', 'Something happened, Success!');
   res.render('index');
 };
 
@@ -11,7 +14,28 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Create ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', {
+    title: `Edit ${store.name}`,
+    store
+  })
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({
+    _id: req.params.id,
+  }, req.body, { new: true, runValidators: true}).exec();
+  req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View Store </a>`);
+  res.redirect(`/stores/${store._id}/edit`);
 };
